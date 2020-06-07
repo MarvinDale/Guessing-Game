41b1c068771e19021c97abece4d4357d1d66521c
MarvinDale <47068005+MarvinDale@users.noreply.github.com>
2020-06-07 16:02:47 +0100

    Update TreeInterface.java

--- TreeInterface.java
+++ TreeInterface.java
@@ -1,1 +1,13 @@
-/** * An interface for basic operations of a tree. *  * @author Frank M. Carrano * @version 2.0 */public interface TreeInterface<T>{  public T getRootData();  public int getHeight();  public int getNumberOfNodes();  public boolean isEmpty();  public void clear();} // end TreeInterface
+/**
+ * An interface for basic operations of a tree
+ * @author Frank M. Carrano
+ * @version 2.0
+ */
+public interface TreeInterface<T>
+{
+  public T getRootData();
+  public int getHeight();
+  public int getNumberOfNodes();
+  public boolean isEmpty();
+  public void clear();
+} // end TreeInterface
